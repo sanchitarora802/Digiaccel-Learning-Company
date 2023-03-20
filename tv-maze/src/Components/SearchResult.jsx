33fd20627e57e056076017ab07455df0665997b5
sanchitarora802 <sanchit.arora802@gmail.com>
2023-03-21 01:21:07 +0530

    ratting added

--- tv-maze/src/Components/SearchResult.jsx
+++ tv-maze/src/Components/SearchResult.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux';
 import "../Css/SearchResult.css";
+import StarRatings from 'react-star-ratings';
 
 function SearchResult() {
+
+
 
 var storeData = useSelector((state) =>{
      return state.Search
      })
  const {searchResult} = storeData
+
+ const changeRating = () =>{
+
+ }
 
   return (
     <div className='searchWrap'>
@@ -17,9 +24,17 @@ var storeData = useSelector((state) =>{
         <div className='ContentWrap'>
           <div className='TitleWrap'>
           <label className='title'>{searchResult.name && searchResult.name}</label>
+        <div className='ratingWrap'>
+          <StarRatings
+          rating={(searchResult.rating.average)/2}
+          starRatedColor="Gold"
+          numberOfStars={5}
+          name='rating'
+        />
+        </div>
           </div>
           <div className='TitleWrap'>
-          <label className='title'>{searchResult.premiered && searchResult.premiered} | {searchResult.runtime && searchResult.runtime}</label>
+          <label className='others'>{searchResult.premiered && searchResult.premiered} | {searchResult.runtime && searchResult.runtime + ' Minutes'}</label>
           </div>
           <div className='SummaryWrap'>
           <label className='SummaryLabel'> Movie Description </label>
